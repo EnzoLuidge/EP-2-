3ab85b0fa817606a7eb8d2c5e2e05b1e1c2d1105
Enzo <enzoluidge4@gmail.com>
2021-04-12 18:15:37 -0300

    Função Extrai Naipe

--- Solitaire Accordion.py
+++ Solitaire Accordion.py
@@ -2,3 +2,9 @@ def cria_baralho():
     lista_baralho = ['A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣']
     print(len(lista_baralho))
     return lista_baralho
+
+def extrai_naipe(carta):
+    lista_naipe = ['♥','♦','♠','♣']
+    for i in lista_naipe:
+        if i in carta:
+            return i
