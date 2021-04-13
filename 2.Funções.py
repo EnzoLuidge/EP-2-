917ea93c38dd5d1841f42c7134882208227f34d1
Enzo <enzoluidge4@gmail.com>
2021-04-13 19:11:02 -0300

    Estado Inicial do Baralho2

--- 2.Funções.py
+++ 2.Funções.py
@@ -1,6 +1,5 @@
 def cria_baralho():
     lista_baralho = ['A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣']
-    print(len(lista_baralho))
     return lista_baralho
 
 def extrai_naipe(carta):
@@ -40,3 +39,13 @@ def empilha(baralho,o,d):
     baralho[d] = baralho[o]
     del baralho[o]
     return baralho
+
+print("O estado atual do baralho é:")
+import random
+i = 1
+lista_baralho = cria_baralho()
+while i < 53:
+    x = ''.join(random.sample(lista_baralho,1))
+    print("{0}.  {1}".format(i,x))
+    del lista_baralho[lista_baralho.index(x)]
+    i +=1
