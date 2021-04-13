d3da3b81f137871298f29b7887f006d69117dbec
Enzo <enzoluidge4@gmail.com>
2021-04-13 19:37:46 -0300

    Escolha de Cartas + Condição se ela pode ser movida ou não

--- 2.Funções.py
+++ 2.Funções.py
@@ -40,12 +40,31 @@ def empilha(baralho,o,d):
     del baralho[o]
     return baralho
 
+def movimento_possivel(cartas,i):
+    i = int(i)
+    if extrai_valor(cartas[i-1]) in cartas[i-2] or extrai_naipe(cartas[i-1]) in cartas[i-2]:
+        return True
+    if extrai_valor(cartas[i-1]) in cartas[i-4] or extrai_naipe(cartas[i-1]) in cartas[i-4]:
+        return True
+    else:
+        return False
+
 print("O estado atual do baralho é:")
 import random
 i = 1
 lista_baralho = cria_baralho()
+novo_baralho = []
 while i < 53:
     x = ''.join(random.sample(lista_baralho,1))
     print("{0}.  {1}".format(i,x))
     del lista_baralho[lista_baralho.index(x)]
+    novo_baralho.append(x)
     i +=1
+    
+numero_escolhido=input("Escolha uma carta (digite um número entre 1 e 52): ")
+movimento= False
+while movimento == False: 
+    if movimento_possivel(novo_baralho,numero_escolhido) == False:
+        numero_escolhido=input("A carta {0} não pode ser movida. Por favor, digite um número entre 1 e 52: ".format(novo_baralho[int(numero_escolhido)-1]))
+    else:
+        movimento = True
